fca71020150220db98884f925422e987381f3f1c
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2022-11-15 11:50:30 +0700

    first commit

---
+++ src/test/java/IDCard/IDCard/IdCardApplicationTests.java
@@ -0,0 +1,13 @@
+package IDCard.IDCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
